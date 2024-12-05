0d7a4ce541513e9ca1fb2d9d30a9f9da822172e7
SnowMar <mnagy78951@gmail.com>
2024-12-05 23:25:20 +0200

    Upload C++ Labs

--- Intro To Progamming/C Course/Lab2/Multipication_Both_Des_Asc/main.c
+++ Intro To Progamming/C Course/Lab2/Multipication_Both_Des_Asc/main.c
@@ -4,33 +4,14 @@
 
 int main() {
 
-    for (int firstLoop = 1; firstLoop <= 10; firstLoop++) {
+    char t [5];
 
-        printf("Multiplication Table for %d:\n", firstLoop);
-        for (int secondLoop = 1; secondLoop <= 10; secondLoop++) {
-            printf("%d x %d = %d\n", firstLoop, secondLoop, firstLoop * secondLoop);
-        }
-
-        if (firstLoop < 10) {
-            printf("\n*****************************\n\n");
-        }
+    for(int i; i < 5; i++){
+        scanf("%c", t[i]);
     }
 
-    printf("\n\nEnd of ascending\n\n");
-
-        for (int firstLoop = 10; firstLoop >= 1; firstLoop--) {
-
-        printf("Multiplication Table for %d:\n", firstLoop);
-        for (int secondLoop = 10; secondLoop >= 1; secondLoop--) {
-            printf("%d x %d = %d\n", firstLoop, secondLoop, firstLoop * secondLoop);
-        }
-
-        if (firstLoop > 10) {
-            printf("\n*****************************\n\n");
-        }
+    for(int i; i < 5; i++){
+        printf("%c", t[i]);
     }
-
-    printf("\n\nEnd of descending\n\n");
-
     return 0;
 }
