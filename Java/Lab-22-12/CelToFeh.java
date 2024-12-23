c9c2fb14ce5acb6ab44b2edea1867e1c7a3f8def
SnowMar <mnagy78951@gmail.com>
2024-12-23 22:57:42 +0200

    NewLabs

--- Java/Lab-22-12/CelToFeh.java
+++
@@ -1,9 +0,0 @@
-import go.*;
-
-public class CelToFeh {
-    public static void main(String[] args) {
-        double celsiusValue = 25;
-        double equation = celtofehequ.getEquation(celsiusValue);
-        System.out.println("Result: " + equation);
-    }
-}
--- Java/Lab-22-12/go/celtofehequ.java
+++
@@ -1,8 +0,0 @@
-package go;
-
-public class celtofehequ {
-    public static double getEquation(double celsius) {
-        return celsius * 9.0 / 5.0 + 32.0;
-    }
-}
-
