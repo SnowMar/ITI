9a96687faa0a1d73df36d5a35b26762378ea0452
SnowMar <mnagy78951@gmail.com>
2024-12-25 22:53:42 +0200

    Java Labs

--- Java/Lab5-24-12/Dictionary/Solution.java
+++ Java/Lab5-24-12/Dictionary/Solution.java
@@ -11,29 +11,24 @@ public class Solution {
             "ximenia", "yuzu", "zuchinni", "another", "butter", "candy", "dandy", "elephant"
         };
 
-        // Populate the map
         for (String fruit : fruits) {
-            String key = String.valueOf(fruit.charAt(0)); // Get the first letter
-            map.putIfAbsent(key, new ArrayList<>()); // Initialize list if key is not present
-            map.get(key).add(fruit); // Add the fruit to the list
+            String key = String.valueOf(fruit.charAt(0));
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(fruit);
         }
 
-        // Display all entries
         displayAll(map);
 
-        // Display entries for a specific letter
         System.out.println("\nEntries starting with 'a':");
         displayOneOnly(map, 'a');
     }
 
-    // Method to display all map entries
     public static void displayAll(TreeMap<String, List<String>> map) {
         for (Map.Entry<String, List<String>> entry : map.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 
-    // Method to display entries starting with a specific letter
     public static void displayOneOnly(TreeMap<String, List<String>> map, char c) {
         String key = String.valueOf(c);
         if (map.containsKey(key)) {
